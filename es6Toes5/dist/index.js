5b6267fb8d0def340220d18805a5d3f35e85e47c
yuanweizhixuan <yuanweizhixuan@outlook.com>
2021-12-28 13:53:08 +0800

    增加radio和checkbox 的样式

--- es6Toes5/dist/index.js
+++ es6Toes5/dist/index.js
@@ -1,102 +1,34 @@
 "use strict";
 
-/**
- * switch 被点击 切换switch 状态和自定义属性的值
- * @param _this 被点击的switch
- */
+function debounce(fn, delay) {
+	var trigger = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
 
-function switchClick(_this) {
-	var value = $(_this).attr("data-value");
-	if (value === "true") {
-		$(_this).attr("data-value", "false");
-		$(_this).children(".x-switch-core").removeClass("is-check");
-	} else {
-		$(_this).attr("data-value", "true");
-		$(_this).children(".x-switch-core").addClass("is-check");
-	}
-}
-
-$(function () {
-	/**
-  * 渲染 Switch到页面中
-  */
-	function renderSwitch() {
-		var xSwitchArr = $(".x-switch");
-		var switchStr = '<span class="x-switch-core"><span class="x-switch-button"></span></span>';
-		for (var i = 0; i < xSwitchArr.length; i++) {
-			$(xSwitchArr[i]).html(switchStr);
-			setDefaultStyle(xSwitchArr[i]);
+	var t = null;
+	var res = null;
+	var debounced = function debounced() {
+		var _self = this;
+		var args = arguments;
+		if (t) {
+			clearTimeout(t);
 		}
-
-		/**
-   * 如果默认就是选中状态的，需要给他添加相应的类并且val的值改为：true
-   * @param  { Element } el 需要设置样式或者取消样式的element元素
-   */
-		function setDefaultStyle(el) {
-			var value = $(el).attr("data-value");
-			//如果默认就是true  则添加 is-check
-			if (value === "true") {
-				$(el).children(".x-switch-core").addClass("is-check");
+		if (trigger) {
+			var exec = !t;
+			t = setTimeout(function () {
+				t = null;
+			}, delay);
+			if (exec) {
+				res = fn.apply(_self, args);
 			}
+		} else {
+			t = setTimeout(function () {
+				res = fn.apply(_self, args);
+			}, delay);
 		}
-	}
-
-	/**
-  * 进入页面初始化调用
-  */
-	function init() {
-		renderSwitch();
-	}
-
-	init();
-});
-
-// /**
-//  * switch 被点击 切换switch 状态和自定义属性的值
-//  * @param _this 被点击的switch
-//  */
-// function switchClick(_this) {
-// 	let value = $(_this).attr("data-value")
-// 	if (value === "true") {
-// 		$(_this).attr("data-value", "false")
-// 		$(_this).children(".x-switch-core").removeClass("is-check")
-// 	} else {
-// 		$(_this).attr("data-value", "true")
-// 		$(_this).children(".x-switch-core").addClass("is-check")
-// 	}
-// }
-//
-// $(function () {
-// 	/**
-// 	 * 渲染 Switch到页面中
-// 	 */
-// 	function renderSwitch() {
-// 		let xSwitchArr = $(".x-switch")
-// 		let switchStr = `<span class="x-switch-core"><span class="x-switch-button"></span></span>`
-// 		for (let i = 0; i < xSwitchArr.length; i++) {
-// 			$(xSwitchArr[i]).html(switchStr)
-// 			setDefaultStyle(xSwitchArr[i])
-// 		}
-//
-// 		/**
-// 		 * 如果默认就是选中状态的，需要给他添加相应的类并且val的值改为：true
-// 		 * @param  { Element } el 需要设置样式或者取消样式的element元素
-// 		 */
-// 		function setDefaultStyle(el) {
-// 			let value = $(el).attr("data-value");
-// 			//如果默认就是true  则添加 is-check
-// 			if (value === "true") {
-// 				$(el).children(".x-switch-core").addClass("is-check")
-// 			}
-// 		}
-// 	}
-//
-// 	/**
-// 	 * 进入页面初始化调用
-// 	 */
-// 	function init() {
-// 		renderSwitch()
-// 	}
-//
-// 	init()
-// })
+		return res;
+	};
+	debounced.remove = function () {
+		clearTimeout(t);
+		t = null;
+	};
+	return debounced;
+}
--- es6Toes5/src/index.js
+++ es6Toes5/src/index.js
@@ -1,102 +1,30 @@
-"use strict";
-
-/**
- * switch 被点击 切换switch 状态和自定义属性的值
- * @param _this 被点击的switch
- */
-function switchClick(_this) {
-	var value = $(_this).attr("data-value");
-	if (value === "true") {
-		$(_this).attr("data-value", "false");
-		$(_this).children(".x-switch-core").removeClass("is-check");
-	} else {
-		$(_this).attr("data-value", "true");
-		$(_this).children(".x-switch-core").addClass("is-check");
-	}
-}
-
-$(function () {
-	/**
-	 * 渲染 Switch到页面中
-	 */
-	function renderSwitch() {
-		var xSwitchArr = $(".x-switch");
-		var switchStr =
-			'<span class="x-switch-core"><span class="x-switch-button"></span></span>';
-		for (var i = 0; i < xSwitchArr.length; i++) {
-			$(xSwitchArr[i]).html(switchStr);
-			setDefaultStyle(xSwitchArr[i]);
+function debounce(fn, delay, trigger = false) {
+	let t = null;
+	let res = null;
+	let debounced = function debounced() {
+		let _self = this;
+		let args = arguments;
+		if (t) {
+			clearTimeout(t);
 		}
-
-		/**
-		 * 如果默认就是选中状态的，需要给他添加相应的类并且val的值改为：true
-		 * @param  { Element } el 需要设置样式或者取消样式的element元素
-		 */
-		function setDefaultStyle(el) {
-			var value = $(el).attr("data-value");
-			//如果默认就是true  则添加 is-check
-			if (value === "true") {
-				$(el).children(".x-switch-core").addClass("is-check");
+		if (trigger) {
+			let exec = !t;
+			t = setTimeout(function () {
+				t = null;
+			}, delay);
+			if (exec) {
+				res = fn.apply(_self, args);
 			}
+		} else {
+			t = setTimeout(function () {
+				res = fn.apply(_self, args);
+			}, delay);
 		}
-	}
-
-	/**
-	 * 进入页面初始化调用
-	 */
-	function init() {
-		renderSwitch();
-	}
-
-	init();
-});
-
-// /**
-//  * switch 被点击 切换switch 状态和自定义属性的值
-//  * @param _this 被点击的switch
-//  */
-// function switchClick(_this) {
-// 	let value = $(_this).attr("data-value")
-// 	if (value === "true") {
-// 		$(_this).attr("data-value", "false")
-// 		$(_this).children(".x-switch-core").removeClass("is-check")
-// 	} else {
-// 		$(_this).attr("data-value", "true")
-// 		$(_this).children(".x-switch-core").addClass("is-check")
-// 	}
-// }
-//
-// $(function () {
-// 	/**
-// 	 * 渲染 Switch到页面中
-// 	 */
-// 	function renderSwitch() {
-// 		let xSwitchArr = $(".x-switch")
-// 		let switchStr = `<span class="x-switch-core"><span class="x-switch-button"></span></span>`
-// 		for (let i = 0; i < xSwitchArr.length; i++) {
-// 			$(xSwitchArr[i]).html(switchStr)
-// 			setDefaultStyle(xSwitchArr[i])
-// 		}
-//
-// 		/**
-// 		 * 如果默认就是选中状态的，需要给他添加相应的类并且val的值改为：true
-// 		 * @param  { Element } el 需要设置样式或者取消样式的element元素
-// 		 */
-// 		function setDefaultStyle(el) {
-// 			let value = $(el).attr("data-value");
-// 			//如果默认就是true  则添加 is-check
-// 			if (value === "true") {
-// 				$(el).children(".x-switch-core").addClass("is-check")
-// 			}
-// 		}
-// 	}
-//
-// 	/**
-// 	 * 进入页面初始化调用
-// 	 */
-// 	function init() {
-// 		renderSwitch()
-// 	}
-//
-// 	init()
-// })
+		return res;
+	};
+	debounced.remove = function () {
+		clearTimeout(t);
+		t = null;
+	};
+	return debounced;
+}
